098946608bce6b7f1d0ec5c15dc2094dd06bb6b4
Mussorable <redduck5601@gmail.com>
2024-11-18 20:40:18 +0100

    starting handling addTruck form

--- src/app/components/vehicles/AddTruck.tsx
+++ src/app/components/vehicles/AddTruck.tsx
@@ -1,26 +1,47 @@
+import {useDispatch, useSelector} from "react-redux";
+import React, {useEffect, useState} from "react";
+import {AppDispatch, RootState} from "../../../store/store.ts";
+import {getDrivers} from "../../../store/drivers.ts";
+
 function AddTruck() {
+  const dispatch: AppDispatch = useDispatch();
+  const drivers = useSelector((state: RootState) => state.drivers.drivers);
+  const [truck, setTruck] = useState({
+    vehicleNumber: '',
+    driverNumber: '',
+    trailerNumber: '',
+  });
+
+  useEffect(() => {
+    dispatch(getDrivers());
+  }, []);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {id, value} = event.target;
+    setTruck({
+      ...truck,
+      [id]: value,
+    });
+  };
+
   return (
     <div className="px-4 pt-4 flex-1">
       <h5 className="font-semibold">Add Truck</h5>
       <form action="" className="mt-2">
         <div className="flex align-middle ml-4 mt-2">
           <label htmlFor="truck-number" className="block self-center w-28">Truck number:</label>
-          <input className="ml-3 px-2 py-1 border-2 border-gray-700 rounded-md focus:outline-none" id="truck-number" type="text" placeholder="7-digit number" />
+          <input value={truck.vehicleNumber} onChange={handleInputChange} className="ml-3 px-2 py-1 border-2 border-gray-700 rounded-md focus:outline-none" id="truck-number" type="text" placeholder="7-digit number" />
         </div>
         <div className="flex aign-middle ml-4 mt-2">
           <label htmlFor="drivers-list" className="block self-center w-28">Current driver:</label>
-          <select className="ml-3 px-2 py-1 border-2 border-gray-700 rounded-md focus:outline-none" name="" id="drivers-list">
+          <select value={truck.driverNumber} className="ml-3 px-2 py-1 border-2 border-gray-700 rounded-md focus:outline-none" name="" id="drivers-list">
             <option value="">Optional</option>
-            <option value="driver-1">driver-1</option>
-            <option value="driver-2">driver-2</option>
-            <option value="driver-3">driver-3</option>
-            <option value="driver-4">driver-4</option>
-            <option value="driver-5">driver-5</option>
+            {drivers && drivers.map((driver) => <option value={driver.number} key={driver.number}>{driver.fullName}</option>)}
           </select>
         </div>
         <div className="flex aign-middle ml-4 mt-2">
           <label htmlFor="trailers-list" className="block self-center w-28">Current trailer:</label>
-          <select className="ml-3 px-2 py-1 border-2 border-gray-700 rounded-md focus:outline-none" name="" id="trailers-list">
+          <select value={truck.trailerNumber} className="ml-3 px-2 py-1 border-2 border-gray-700 rounded-md focus:outline-none" name="" id="trailers-list">
             <option value="">Optional</option>
             <option value="trailer-1">trailer-1</option>
             <option value="trailer-2">trailer-2</option>
